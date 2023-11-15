afc4ba32b53a9b90d46d3d209c17826db5c1dd6a
Figar Januar <figar@investree.id>
2023-11-15 14:51:18 +0700

    fix find match

--- src/views/PageIndex.vue
+++ src/views/PageIndex.vue
@@ -35,32 +35,34 @@ export default {
   },
   methods: {
     async initMatch() {
-      const loginStore = useLoginStore()
-      const queueDocRef = doc(this.$db, "match-queue", 'gor-1')
-      const matchListDocRef = doc(this.$db, "match-list", loginStore.userData.user.uid)
-      const matchStore = useMatchStore()
+      const loginStore = useLoginStore();
+      const queueDocRef = doc(this.$db, "match-queue", 'gor-1');
+      const matchListDocRef = doc(this.$db, "match-list", loginStore.userData.user.uid);
+      const matchStore = useMatchStore();
 
       // Hanya mendengarkan snapshot jika sedang mencari pertandingan
       onSnapshot(queueDocRef, async (snapshot) => {
-        const source = snapshot.metadata.hasPendingWrites ? "Local" : "Server"
-        const data = snapshot.data()
+        const data = snapshot.data();
 
         // Hanya melanjutkan jika sedang mencari pertandingan
         if (data) {
-          const player1 = data[loginStore.userData.user.uid]
-          const players = Object.keys(data).filter(player => player !== loginStore.userData.user.uid)
+          const player1 = data[loginStore.userData.user.uid];
+          const players = Object.keys(data).filter(player => player !== loginStore.userData.user.uid);
+
+          // Track if a match is found
+          let matchFound = false;
 
           for (const player of players) {
-            const player2 = data[player]
+            const player2 = data[player];
 
-            const diffScore = Math.abs(player1?.score - player2?.score)
+            const diffScore = Math.abs(player1?.score - player2?.score);
 
             if (
               diffScore <= 10 &&
               player1.gender === player2.gender &&
-              player1.venue === player2.venue
+              player1.venue === player2.venue &&
+              !matchFound  // Check if a match is not already found
             ) {
-
               await runTransaction(this.$db, async (transaction) => {
                 const docSnap = await transaction.get(queueDocRef);
 
@@ -73,30 +75,30 @@ export default {
                   delete updatedData[player];
                   transaction.set(queueDocRef, updatedData);
                 }
-              })
+              });
 
-              const matchListSnap = await getDoc(matchListDocRef)
-              const matchListData = matchListSnap.exists() ? matchListSnap.data().matches || [] : []
+              const matchListSnap = await getDoc(matchListDocRef);
+              const matchListData = matchListSnap.exists() ? matchListSnap.data().matches || [] : [];
 
               // Menyimpan data player2 saja sebagai opponent
               matchListData.push({
                 venue: player1.venue,
                 date: new Date(),
                 opponent: player2,  // Hanya menyimpan data player2
-                status: 'berlangsung'
-              })
+                status: 'berlangsung',
+              });
 
-              await setDoc(matchListDocRef, { matches: matchListData })
+              await setDoc(matchListDocRef, { matches: matchListData });
 
-              matchStore.isFinding(false)
-              this.$router.push({ name: 'my-match' })
-              return // Hentikan pencarian setelah menemukan lawan
+              matchFound = true;  // Set matchFound to true
+              matchStore.isFinding(false);
+              this.$router.push({ name: 'my-match' });
+              break; // Hentikan pencarian setelah menemukan lawan
             }
           }
         }
-      })
+      });
     }
-
   },
   mounted() {
     this.initMatch()
